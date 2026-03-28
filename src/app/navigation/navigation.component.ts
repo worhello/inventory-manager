64a20a28e8915018f99b8dfde6ceab63ad7e5957
worhello <worhello@gmail.com>
2026-03-28 12:35:46 +0000

    Navbar closes are clicking on a link

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,14 +1,14 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { AsyncPipe } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { filter, map, shareReplay, withLatestFrom } from 'rxjs/operators';
+import { NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
@@ -21,13 +21,29 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
     MatListModule,
     MatIconModule,
     AsyncPipe,
-    RouterOutlet, RouterLink, RouterLinkActive]
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive,
+  ],
 })
 export class NavigationComponent {
+  @ViewChild('drawer') drawer: MatSidenav = {} as MatSidenav;
+
   private breakpointObserver = inject(BreakpointObserver);
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map((result) => result.matches),
     shareReplay(),
   );
+
+  constructor() {
+    const router = inject(Router);
+
+    router.events
+      .pipe(
+        withLatestFrom(this.isHandset$),
+        filter(([a, b]) => b && a instanceof NavigationEnd),
+      )
+      .subscribe(() => this.drawer.close());
+  }
 }
