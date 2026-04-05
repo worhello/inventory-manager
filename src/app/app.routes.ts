190651477f9ecc549db3de105ecccacced3bc31b
worhello <worhello@gmail.com>
2026-04-05 21:32:19 +0100

    Add About page to UI as bottom option

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { CookingView } from './cooking-view/cooking-view';
 import { InventoryView } from './inventory-view/inventory-view';
 import { ShoppingView } from './shopping-view/shopping-view';
 import { Settings } from './settings/settings';
+import { About } from './about/about';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/inventory', pathMatch: 'full' },
@@ -10,4 +11,5 @@ export const routes: Routes = [
   { path: 'shopping', component: ShoppingView, data: {title: 'Shopping'} },
   { path: 'cooking', component: CookingView, data: {title: 'Cooking'} },
   { path: 'settings', component: Settings, data: {title: 'Settings'} },
+  { path: 'about', component: About, data: {title: 'About'} },
 ];
